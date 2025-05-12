aab42a4628d46e51fa0544928773ce69b0da7476
lonewanderer27 <adrianejames27@gmail.com>
2025-05-13 00:57:29 +0800

    feat(Empty): add animation as custom prop, set coffee machine by default

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -3,10 +3,10 @@ import { DotLottiePlayer } from "@dotlottie/react-player";
 import { IonText } from "@ionic/react";
 import { memo } from "react";
 
-const Empty = (props: { title: string; description: string }) => {
+const Empty = (props: { title: string; description: string, animation: string }) => {
   return (
     <div id="notice" className="p-5">
-      <DotLottiePlayer src={CoffeeMachine} autoplay loop className="-mt-40">
+      <DotLottiePlayer src={props.animation} autoplay loop className="-mt-40">
         <div className="mt-[-50px] z-10">
           <IonText>
             <h3 className="font-bold">Your {props.title} is empty</h3>
@@ -24,7 +24,8 @@ const Empty = (props: { title: string; description: string }) => {
 
 Empty.defaultProps = {
   title: "cart",
-  description: "Load up that basket with our premium selection of goods!"
+  description: "Load up that basket with our premium selection of goods!",
+  animation: CoffeeMachine
 };
 
 export default memo(Empty);
